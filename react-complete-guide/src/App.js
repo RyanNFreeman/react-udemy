259586343d6bc848aa8a5c20653cc9f6d2297144
Joe Bugajski <yogurtmystery@gmail.com>
2019-02-20 14:05:48 -0600

    -added comments about default imports -added JSX cheatsheet to README.md

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Banana from './Person/Person';
+// you can import default exports as anything you wish, but remember to capitalize
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Banana />
       </div>
     );
     // This is what it looks like without JSX:
